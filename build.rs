07ec959d8582f18d641e3e663aa7c429c8574fc5
Tyler Thompson <yoitscore@gmail.com>
2023-11-10 15:04:02 +0000

    Fixed Linux building

--- build.rs
+++ build.rs
@@ -1,11 +1,8 @@
-use {
-    std::{env, io},
-    winres::WindowsResource,
-};
+use std::{env, io};
 
 fn main() -> io::Result<()> {
     if env::var_os("CARGO_CFG_WINDOWS").is_some() {
-        WindowsResource::new()
+        winres::WindowsResource::new()
             .set_icon("assets/icon.ico")
             .compile()?;
     }
